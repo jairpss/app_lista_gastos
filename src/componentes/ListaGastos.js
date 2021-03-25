3c47871b0425f4a32b4c318c174442beaa3af3fc
Jair Partida <jairpartida1@gmail.com>
2021-03-24 23:01:04 -0700

    Creando el hook para obtener gastos

--- src/componentes/ListaGastos.js
+++ src/componentes/ListaGastos.js
@@ -3,9 +3,11 @@ import {Header, Titulo} from './../elementos/Header';
 import {Helmet} from 'react-helmet';
 import BtnRegresar from './../elementos/BtnRegresar';
 import BarraTotalGastos from './BarraTotalGastos';
+import useObtenerGastos from './../hooks/useObtenerGastos';
 
 const ListaGastos = () => {
-
+    const gastos = useObtenerGastos();
+    console.log(gastos);
     return ( 
         <>
             <Helmet>
