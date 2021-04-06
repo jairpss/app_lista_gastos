c181c870d340a9cde8229c5274dbafc23a462e18
Jair Partida <jairpartida1@gmail.com>
2021-04-05 22:33:00 -0600

    creando el hook para obtener gastos del mes

---
+++ src/hooks/useObtenerGastoMes.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+import {db} from './../firebase/firebaseConfig';
+import {startOfMonth, endOfMonth, getUnixTime} from 'date-fns';
+import {useAuth} from './../contextos/AuthContext';
+
+const useObtenerGastoMes = (id) => {
+    const [gastos, establecerGastos] = useState('');
+    const {usuario} = useAuth();
+    
+    useEffect(() => {
+        const inicioDeMes = getUnixTime(startOfMonth(new Date()));
+        const finDeMes = getUnixTime(endOfMonth(new Date()));
+
+        if(usuario) {
+            const unsuscribe = db.collection('gastos')
+            .orderBy('fecha', 'desc')
+            .where('fecha', '>=', inicioDeMes)
+            .where('fecha', '<=', finDeMes)
+            .where('uidUsuario', '==', usuario.uid)
+            .onSnapshot((snapshot) => {
+                establecerGastos(snapshot.docs.map((documento) => {
+                    return {...documento.data(), id: documento.id}
+                }))
+            })
+            //Use effect retorna una funcion cuando se desmonte el componente
+            //en esete caso queremos que ejecute el unsuscribe a la coleccion de firestore
+            return unsuscribe;
+        }
+    }, [usuario]);
+
+    return [gastos];
+}
+ 
+export default useObtenerGastoMes;
